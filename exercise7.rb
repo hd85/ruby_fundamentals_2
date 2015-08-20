17f6ce07ca5a164ef224f6f497d08974f02e3d48
hd85 <hdossa01@gmail.com>
2015-08-20 00:08:41 -0400

    adding code for all steps in exercise 7

--- exercise7.rb
+++ exercise7.rb
@@ -1,3 +1,9 @@
+def display_students (students)
+  students.each do |key, value|
+    puts "#{key}: #{value} students"
+  end
+end
+
 students = {
   :cohort1 => 34,
   :cohort2 => 42,
@@ -8,3 +14,12 @@ students = {
 students.each do |cohort, value|
   puts "#{cohort}: #{value} students"
 end
+
+students.each do |cohort, value|
+  value = (value * 1.05)
+    puts "#{cohort}: #{value} students"
+end
+
+students.delete(:cohort2)
+
+display_students(students)
